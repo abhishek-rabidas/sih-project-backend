b5d439e2a55c029b5755fc3c2442ed17913a045d
abhishek <avisec07@gmail.com>
2022-07-06 11:51:41 +0530

    annotation fix

--- src/main/java/org/aicte/sih/SIHProject/api/college/services/CollegeServiceImplementation.java
+++ src/main/java/org/aicte/sih/SIHProject/api/college/services/CollegeServiceImplementation.java
@@ -3,10 +3,12 @@ package org.aicte.sih.SIHProject.api.college.services;
 import org.aicte.sih.SIHProject.api.college.Repository.CollegeRepository;
 import org.aicte.sih.SIHProject.api.college.dto.entities.CollegeEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
 
+@Service
 public class CollegeServiceImplementation implements CollegeService{
 
 
