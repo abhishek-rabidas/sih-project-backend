311605ab0af1b32152eefbbdd997e67f438e36fe
siddheswarojha <siddheswarojha23@gmail.com>
2022-08-22 04:39:08 +0530

    CRUD for experience

--- src/main/java/org/aicte/sih/SIHProject/api/experience/controller/FacultyExperienceController.java
+++ src/main/java/org/aicte/sih/SIHProject/api/experience/controller/FacultyExperienceController.java
@@ -1,17 +1,17 @@
 package org.aicte.sih.SIHProject.api.experience.controller;
 
+import org.aicte.sih.SIHProject.api.certificate.dto.Entity.FacultyCertificate;
 import org.aicte.sih.SIHProject.api.experience.dto.entity.FacultyExperience;
 import org.aicte.sih.SIHProject.api.experience.dto.request.FacultyExperienceAddRequest;
 import org.aicte.sih.SIHProject.api.experience.services.FacultyExperienceService;
 import org.aicte.sih.SIHProject.commons.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/experience")
@@ -25,6 +25,56 @@ public class FacultyExperienceController {
         APIResponse<FacultyExperience> response = new APIResponse<>();
         try {
             response.setData(facultyExperienceService.addNewExperience(facultyExperienceAddRequest));
+            return ResponseEntity.ok(response);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            response.setStatusCode(e.getStatusCode().value());
+            response.setMessage(e.getStatusText());
+            return ResponseEntity.internalServerError().body(response);
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage(e.getMessage());
+            return ResponseEntity.internalServerError().body(response);
+        }
+    }
+    @GetMapping("/getFacultyExperience")
+    public ResponseEntity<APIResponse<List<FacultyExperience>>> getExperienceForFaculty(@PathVariable("id") Long id) {
+        APIResponse<List<FacultyExperience>> response = new APIResponse<>();
+        try {
+            response.setData(facultyExperienceService.listAllExperience(id));
+            return ResponseEntity.status(response.getStatusCode()).body(response);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            response.setStatusCode(e.getStatusCode().value());
+            response.setMessage(e.getStatusText());
+            return ResponseEntity.internalServerError().body(response);
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("internal server error");
+            return ResponseEntity.internalServerError().body(response);
+        }
+    }
+
+    @PostMapping("/closeExperience/{id}")
+    public ResponseEntity<APIResponse> deleteExperience(@PathVariable("id") Long id) {
+        APIResponse response = new APIResponse<>();
+        try {
+            facultyExperienceService.deleteExperience(id);
+            return ResponseEntity.ok(response);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            response.setStatusCode(e.getStatusCode().value());
+            response.setMessage(e.getStatusText());
+            return ResponseEntity.internalServerError().body(response);
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage(e.getMessage());
+            return ResponseEntity.internalServerError().body(response);
+        }
+    }
+
+    @PutMapping("/updateExperience/{id}")
+    public ResponseEntity<APIResponse> updateExperience(@PathVariable("id") Long id, @RequestBody FacultyExperience facultyExperience) {
+        APIResponse response = new APIResponse<>();
+        try {
+            facultyExperienceService.editExperience(id, facultyExperience);
             return ResponseEntity.ok(response);
         } catch (HttpClientErrorException | HttpServerErrorException e) {
             response.setStatusCode(e.getStatusCode().value());
--- src/main/java/org/aicte/sih/SIHProject/api/experience/services/FacultyExperienceServiceImplementation.java
+++ src/main/java/org/aicte/sih/SIHProject/api/experience/services/FacultyExperienceServiceImplementation.java
@@ -1,32 +1,72 @@
 package org.aicte.sih.SIHProject.api.experience.services;
 
+import org.aicte.sih.SIHProject.api.certificate.dto.Entity.FacultyCertificate;
+import org.aicte.sih.SIHProject.api.certificate.exceptions.FacultyCertificateException;
+import org.aicte.sih.SIHProject.api.college.Exception.CollegeExceptions;
+import org.aicte.sih.SIHProject.api.college.dto.entities.CollegeEntity;
+import org.aicte.sih.SIHProject.api.experience.dao.FacultyExperienceRepository;
 import org.aicte.sih.SIHProject.api.experience.dto.entity.FacultyExperience;
 import org.aicte.sih.SIHProject.api.experience.dto.request.FacultyExperienceAddRequest;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Locale;
+
 @Service
 public class FacultyExperienceServiceImplementation implements FacultyExperienceService {
 
+
+    @Autowired
+    private FacultyExperienceRepository facultyExperienceRepository;
+
     @Override
     public FacultyExperience addNewExperience(FacultyExperienceAddRequest request) {
-        return null;
+
+        FacultyExperience facultyExperience = new FacultyExperience();
+        facultyExperience.setOverview(request.getOverview());
+        facultyExperience.setLocation(request.getLocation());
+        facultyExperience.setSkills(request.getSkills());
+        facultyExperience.setOrganization(request.getOrganization());
+        facultyExperience.setEndMonth(request.getEndMonth());
+        facultyExperience.setStartYear(request.getStartYear());
+        facultyExperience.setEndYear(request.getEndYear());
+        facultyExperience.setStartMonth(request.getStartMonth());
+        return facultyExperienceRepository.save(facultyExperience);
     }
 
     @Override
-    public FacultyExperience editExperience(FacultyExperienceAddRequest request) {
-        return null;
+    public FacultyExperience editExperience(Long id, FacultyExperience request) {
+        FacultyExperience experience = facultyExperienceRepository.findOneById(id);
+        if (experience == null) {
+            throw new FacultyCertificateException("Certificate Not Found");
+        } else {
+
+            experience.setSkills(request.getSkills());
+            experience.setOrganization(request.getOrganization());
+            experience.setLocation(request.getLocation());
+            experience.setEndYear(request.getEndYear());
+            experience.setStartYear(request.getStartYear());
+            experience.setStartMonth(request.getStartMonth());
+            experience.setEndMonth(request.getEndMonth());
+            experience.setOverview(request.getOverview());
+
+            return facultyExperienceRepository.save(experience);
+        }
     }
 
     @Override
-    public void deleteExperience(Long facultyId, Long experienceId) {
+    public void deleteExperience(Long experienceId) {
+        facultyExperienceRepository.deleteById(experienceId);
 
     }
 
     @Override
-    public Page<FacultyExperience> listAllExperience(Long facultyId, PageRequest pageRequest) {
-        return null;
+    public List<FacultyExperience> listAllExperience(Long id) {
+        return facultyExperienceRepository.findAllByFaculty(facultyExperienceRepository.findOneById(id));
     }
 
     @Override
